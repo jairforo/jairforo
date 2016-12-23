4965b68bdf474b666546393bd17a96cf019a0ece
Jair Foro <jairforo@gmail.com>
2016-12-23 17:14:59 -0200

    ajustes do gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp = require('gulp')
   ,imagemin = require('gulp-imagemin')
   ,clean = require('gulp-clean')
-  ,concat = require('gulp-concat')
-  ,htmlReplace = require('gulp-html-replace')
   ,uglify = require('gulp-uglify')
   ,usemin = require('gulp-usemin')
   ,cssmin = require('gulp-cssmin')
@@ -11,7 +9,7 @@ var gulp = require('gulp')
   ,jshintStylish = require('jshint-stylish')
   ,csslint = require('gulp-csslint')
   ,autoprefixer = require('gulp-autoprefixer')
-  ,less = require('gulp-less');
+  ,sass = require('gulp-sass');
 
 gulp.task('default', ['copy'], function() {
 	gulp.start('build-img', 'usemin');
@@ -29,16 +27,17 @@ gulp.task('clean', function() {
 
 gulp.task('build-img', function() {
 
-  return gulp.src('dist/img/**/*')
+  return gulp.src('src/assets/images/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+    .pipe(gulp.dest('dist/assets/images'));
 });
 
 gulp.task('usemin', function() {
+
   return gulp.src('dist/**/*.html')
     .pipe(usemin({
       js: [uglify],
-      css: [autoprefixer]
+      css: [autoprefixer, cssmin]
     }))
     .pipe(gulp.dest('dist'));
 });
@@ -52,26 +51,26 @@ gulp.task('server', function() {
 
     gulp.watch('src/**/*').on('change', browserSync.reload);
 
-    gulp.watch('src/js/**/*.js').on('change', function(event) {
+    gulp.watch('src/assets/js/**/*.js').on('change', function(event) {
         console.log("Linting " + event.path);
         gulp.src(event.path)
             .pipe(jshint())
             .pipe(jshint.reporter(jshintStylish));
     });
 
-    gulp.watch('src/css/**/*.css').on('change', function(event) {
+    gulp.watch('src/assets/css/**/*.css').on('change', function(event) {
         console.log("Linting " + event.path);
         gulp.src(event.path)
             .pipe(csslint())
             .pipe(csslint.reporter());
-    }); 
+    });
 
-    gulp.watch('src/less/**/*.less').on('change', function(event) {
+    gulp.watch('src/assets/sass/**/*.scss').on('change', function(event) {
        var stream = gulp.src(event.path)
-            .pipe(less().on('error', function(erro) {
+            .pipe(sass().on('error', function(erro) {
               console.log('LESS, erro compilação: ' + erro.filename);
               console.log(erro.message);
             }))
-            .pipe(gulp.dest('src/css'));
-    });   
+            .pipe(gulp.dest('src/assets/css'));
+    });
 });
