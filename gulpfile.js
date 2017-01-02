8eb8b68125f621f9bd08facfd388ddc64619412c
INTRANET\jair.santos <jair.santos@faroseducacional.com.br>
2017-01-02 15:37:18 -0200

    Ajustes de Gulp e Alteração de texto

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,14 @@ var gulp = require('gulp')
   ,imagemin = require('gulp-imagemin')
   ,clean = require('gulp-clean')
   ,uglify = require('gulp-uglify')
-  ,usemin = require('gulp-usemin')
+  ,useref = require('gulp-useref')
+  ,gulpif = require('gulp-if')
   ,cssmin = require('gulp-cssmin')
   ,browserSync = require('browser-sync').create()
-  ,jshint = require('gulp-jshint')
-  ,jshintStylish = require('jshint-stylish')
-  ,csslint = require('gulp-csslint')
-  ,autoprefixer = require('gulp-autoprefixer')
-  ,sass = require('gulp-sass');
+  ,autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', ['copy'], function() {
-	gulp.start('build-img', 'usemin');
+	gulp.start('build-img', 'useref');
 });
 
 gulp.task('copy', ['clean'], function() {
@@ -34,14 +31,13 @@ gulp.task('build-img', function() {
     .pipe(gulp.dest('dist/assets/images'));
 });
 
-gulp.task('usemin', function() {
-
-  return gulp.src('src/**/*.html')
-    .pipe(usemin({
-      js: [uglify],
-      css: [autoprefixer, cssmin]
-    }))
-    .pipe(gulp.dest('dist'));
+gulp.task('useref', function() {
+    return gulp.src('src/**/*.html')
+        .pipe(useref())
+        .pipe(gulpif('src/**/*.js', uglify()))
+        .pipe(gulpif('src/**/*.css', autoprefixer()))
+        .pipe(gulpif('src/**/*.css', cssmin()))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('server', function() {
@@ -52,27 +48,4 @@ gulp.task('server', function() {
     });
 
     gulp.watch('src/**/*').on('change', browserSync.reload);
-
-    gulp.watch('src/assets/js/**/*.js').on('change', function(event) {
-        console.log("Linting " + event.path);
-        gulp.src(event.path)
-            .pipe(jshint())
-            .pipe(jshint.reporter(jshintStylish));
-    });
-
-    gulp.watch('src/assets/css/**/*.css').on('change', function(event) {
-        console.log("Linting " + event.path);
-        gulp.src(event.path)
-            .pipe(csslint())
-            .pipe(csslint.reporter());
-    });
-
-    gulp.watch('src/assets/sass/**/*.scss').on('change', function(event) {
-       var stream = gulp.src(event.path)
-            .pipe(sass().on('error', function(erro) {
-              console.log('LESS, erro compilação: ' + erro.filename);
-              console.log(erro.message);
-            }))
-            .pipe(gulp.dest('src/assets/css'));
-    });
 });
